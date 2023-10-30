8c4e020e617ce3d70f112585c528f31b82653de6
bodaue <timka227@mail.ru>
2023-10-30 16:50:40 +0300

    new updates

--- tgbot/downloaded_files/folders/Тест4/Robot_For_Student (2019_2020)/BrokenRobotGameEmpty v3_3/src/brokenrobotgame/model/events/RobotActionListener.java
+++
@@ -1,7 +0,0 @@
-package brokenrobotgame.model.events;
-
-import java.util.EventListener;
-
-public interface RobotActionListener extends EventListener {
-    void robotMakedMove(RobotActionEvent e);
-}
